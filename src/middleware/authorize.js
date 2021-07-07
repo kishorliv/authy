de22257bcbc8c959da4750751dbc5d8698ea3e68
kishor <kishorliv@gmail.com>
2021-07-07 23:14:00 +1000

    Return function reference instead of a promise.

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -5,19 +5,30 @@ const db = require('../helpers/db');
 // eslint-disable-next-line no-use-before-define
 module.exports = authorize;
 
-async function authorize(req, res, next) {
-  if (!req.headers || !req.headers.authorization) {
-    return res.status(401).json({ message: 'Auth token not found!' });
-  }
+function authorize() {
+  // eslint-disable-next-line no-use-before-define
+  return async (req, res, next) => {
+    if (!req.headers || !req.headers.authorization) {
+      return res
+        .status(401)
+        .json({ error: true, message: 'Auth token not found!' });
+    }
 
-  const token = req.headers.authorization.split(' ')[1];
-  const decodedToken = jwt.verify(token, process.env.SECRET_JWT);
-  const user = await db.Account.findById({ id: decodedToken.id });
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_JWT);
+    const user = await db.Account.findById({ id: decodedToken.id });
 
-  if (!user) {
-    return res.status(401).json({ message: 'Unauthorized!' });
-  }
+    if (!user) {
+      return res.status(401).json({ error: true, message: 'Unauthorized!' });
+    }
 
-  req.user = user; // attach user obj to the request
-  next();
+    const refreshTokens = await db.RefreshToken.find({ account: user.id });
+
+    // attach user obj to the request
+    req.user = user;
+    // eslint-disable-next-line no-shadow
+    req.user.ownsToken = (decodedToken) =>
+      !!refreshTokens.find((x) => x.token === decodedToken);
+    next();
+  };
 }
