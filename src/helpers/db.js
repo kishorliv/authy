e04e5d129390d0b8e044f7324d659105a251f007
kishor <kishorliv@gmail.com>
2021-07-11 19:56:07 +1000

    Remove unnecessary comments.

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -13,14 +13,4 @@ function connectDB() {
   });
 }
 
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: true,
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
 module.exports = { connectDB, Account, RefreshToken };
