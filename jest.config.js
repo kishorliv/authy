d4a5f36970a2a31919581773e4489a2f7b333283
kishor <kishorliv@gmail.com>
2021-07-11 19:22:30 +1000

    Configure jest to read env variables from .env.test.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironmentOptions: {
     NODE_ENV: 'test',
   },
+  setupFiles: ['tests/setup-tests.js'],
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/app.js', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
