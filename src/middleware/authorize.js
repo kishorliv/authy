dd1e05bcb2b69a26eb00c7c31bab49812544c728
kishor <kishorliv@gmail.com>
2021-07-16 17:04:51 +1000

    Check for null and expired bearer tokens.

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -5,6 +5,7 @@ const db = require('../helpers/db');
 module.exports = authorize;
 
 function authorize() {
+  let decodedToken;
   /* eslint-disable consistent-return */
   return async (req, res, next) => {
     if (!req.headers || !req.headers.authorization) {
@@ -14,11 +15,27 @@ function authorize() {
     }
 
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_JWT);
+    // handle Authorization: Bearer null
+    if (token === 'null') {
+      return res
+        .status(401)
+        .json({ error: true, message: 'Unauthorized! Bearer null!' });
+    }
+
+    try {
+      decodedToken = jwt.verify(token, process.env.SECRET_JWT);
+    } catch (error) {
+      return res
+        .status(401)
+        .json({ error: true, message: 'Unauthorized! Token expired!' });
+    }
+
     const user = await db.Account.findById({ id: decodedToken.id });
 
     if (!user) {
-      return res.status(401).json({ error: true, message: 'Unauthorized!' });
+      return res
+        .status(401)
+        .json({ error: true, message: 'Unauthorized! User not found!' });
     }
 
     const refreshTokens = await db.RefreshToken.find({ account: user.id });
